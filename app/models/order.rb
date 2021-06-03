4e9d617c5cce26e1893086b3db90d755830c21ed
akira-iguchi <akituri931@icloud.com>
2021-06-03 11:42:15 +0900

    react追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ApplicationRecord
 
   validates :total_price, numericality: { greater_than: 0 }
 
+  # トランザクションはトランザクション内で例外が発生した場合にロールバックをするという特徴（save!など）
   def save_with_update_line_foods!(line_foods)
     ActiveRecord::Base.transaction do
       line_foods.each do |line_food|
